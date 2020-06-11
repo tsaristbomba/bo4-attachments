7e15fd2468fdf1fd46d3bfa244373bddb52f5e1d
tsaristbomba <andrexerdna@gmail.com>
2020-06-11 10:51:12 -0300

    Updates

---
+++ precache-manifest.755cd77ff12bfe0f9e05acc8e8c9a54f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90e57a897702fabc747892a6581b586",
+    "url": "/bo4-attachments/index.html"
+  },
+  {
+    "revision": "1f812a8b25d5795bf889",
+    "url": "/bo4-attachments/static/css/main.baa20cd4.chunk.css"
+  },
+  {
+    "revision": "08de82306a415ef259d0",
+    "url": "/bo4-attachments/static/js/2.2b1dcd75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bo4-attachments/static/js/2.2b1dcd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f812a8b25d5795bf889",
+    "url": "/bo4-attachments/static/js/main.0f8ae990.chunk.js"
+  },
+  {
+    "revision": "dffd4b964347bab7862b",
+    "url": "/bo4-attachments/static/js/runtime-main.39f88e77.js"
+  }
+]);
